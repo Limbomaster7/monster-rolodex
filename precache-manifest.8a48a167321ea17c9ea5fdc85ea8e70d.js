653c40c49caf67271d154b63d6356e7ddfc20536
Jin Kim <Limbomaster7@gmail.com>
2019-07-31 19:15:24 +0900

    Updates

---
+++ precache-manifest.8a48a167321ea17c9ea5fdc85ea8e70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26312c7c815197867f41af7ef564db6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4c82399ae73794bb72ed",
+    "url": "/monster-rolodex/static/css/main.326e5f3e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4c82399ae73794bb72ed",
+    "url": "/monster-rolodex/static/js/main.01c03071.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
